47b8612a564a1c5020308322c8c8be18bc1fb60a
champmahfuz <mahfuzrahman517@gmail.com>
2024-12-27 11:21:16 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold ">Hello world!</h1>
+      </div>
+      <div className="p-10">
+        <button className="btn">Click Me</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
